86dac8f3d22a78f6c0f458c1db68c87a12e0b8bf
Charles <charles.fischer17@gmail.com>
2023-02-27 09:04:02 -0300

    js

---
+++ JavaScript/Aula 1/Musica.js
@@ -0,0 +1,8 @@
+let musica
+
+musica1 = "Can you hear the silence?\n"
+musica2 = "Can you see the dark?\n"
+musica3 = "Can you fix the broken?\n"
+musica4 = "Can you feel, can you feel my heart?\n"
+
+console.log(musica1 + musica2 + musica3 + musica4)
---
+++ JavaScript/Aula 1/Subtracao.js
@@ -0,0 +1,5 @@
+let x
+
+x = 10 - "2"
+
+console.log(x)
